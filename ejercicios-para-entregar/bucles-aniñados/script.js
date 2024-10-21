15f70cdab7482a9f575480063935e36dd8a7bf0d
Efrén Corzón Vázquez <efcovaz@gmail.com>
2024-10-21 11:22:45 +0200

    bucles-aniñados

---
+++ ejercicios-para-entregar/bucles-aniñados/script.js
@@ -0,0 +1,33 @@
+let txt = prompt("Introduce unha cadea de texto para quitarlle as vocais:");
+
+let palabras = txt.split(" ");
+
+let textoSenVocais = "";
+
+let i = 0;
+
+while (i < palabras.length) {
+  let palabra = palabras[i];
+
+  for (let h = 0; h < palabra.length; h++) {
+    let letra = palabra[h];
+
+    if (
+      letra !== "a" &&
+      letra !== "e" &&
+      letra !== "i" &&
+      letra !== "o" &&
+      letra !== "u"
+    ) {
+      textoSenVocais += palabra[h];
+    }
+  }
+
+  if (i < palabras.length - 1) {
+    textoSenVocais += " ";
+  }
+
+  i++;
+}
+
+alert(textoSenVocais);
